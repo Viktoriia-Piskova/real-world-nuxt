e18f3d126692709d2de4d6c3f92f111941b12385
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-11-17 19:17:02 +0200

    Title and description for SEO add

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,21 @@
     <h1>All Events</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() { // <-- property used by vue-meta to add header tags
+    return {
+      title: 'Event Listing - Real World Events', // <-- For our title tag
+      meta: [
+        {
+          hid: 'description',  
+          name: 'description', // <-- for our meta description tag
+          content:
+            'Where you can find all the events taking place in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
