5454ff148f1ef2ac0e144eb90e7f4b589d66d09b
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-11-18 17:21:12 +0200

    API call using nuxt hook and axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    <h1>All Events</h1>
+    <h1>All Events list</h1>
+
+    <EventCard
+    v-for="(event, index) in events"
+    :key="index"
+    :event="event"
+    :data-index="index"
+
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '~/components/EventCard.vue'
 export default {
+  components:{
+    EventCard
+  },
+  asyncData(context){ // asyncData is a hook provided by nuxt in every .vue inside 'pages'
+    return context.$axios.get('http://localhost:3000/events').then(response => {
+      return {
+        events: response.data
+      }
+    })
+  },
   head() { // <-- property used by vue-meta to add header tags
     return {
       title: 'Event Listing', // <-- For our title tag
